7a11f27170d87221f82217467ed842d3ac4b726d
vbradara <vedran.bradara@outlook.com>
2023-10-21 18:01:55 +0200

    variables

---
+++ java-basics/src/com/mperic/Main.java
@@ -0,0 +1,12 @@
+package com.mperic;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
---
+++ java-basics/src/com/mperic/variables/Variables.java
@@ -0,0 +1,13 @@
+package com.mperic.variables;
+
+public class Variables {
+    boolean bb = true;
+    byte b = 127;
+    short s = 63;
+    int i = 1;
+    long l = 1;
+    float f = 1.1f;
+    double d = 1.2d;
+    int[] arr = new int[] {1,2,4};
+
+}
